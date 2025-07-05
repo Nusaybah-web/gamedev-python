f3b3f9cd2fe4754f23af2d2734e7ec3e7ee29efc
Nusaybah-web <nusaybahfatin@gmail.com>
2025-07-05 10:14:37 +0200

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+
+
+my_class={'Anna':[1,random.randint(50,100)],'Bella':[2,random.randint(50,100)],'Cindy':[3,random.randint(50,100)],'Derik':[4,random.randint(50,100)],'Ella':[5,random.randint(50,100)],'Filip':[6,random.randint(50,100)],'Gerald':[7,random.randint(50,100)],'Hannah':[8,random.randint(50,100)],'Jack':[9,random.randint(50,100)],'Kate':[10,random.randint(50,100)],'Liam':[11,random.randint(50,100)],'Markus':[12,random.randint(50,100)],'Nina':[13,random.randint(50,100)],'Oscar':[14,random.randint(50,100)]}
+
+
+for key,value in my_class.items():
+    print(key,value)
