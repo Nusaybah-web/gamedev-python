9c675fa553fdc5e2a5b7f879fd516138a73640b4
Nusaybah-web <nusaybahfatin@gmail.com>
2025-07-19 10:14:52 +0200

    Add files via upload

--- aliengame.py
+++ aliengame.py
@@ -10,6 +10,7 @@ alien.pos=300,250
 
 text="game start"
 
+aliendirection=1
 
 def draw():
     screen.fill("dark blue")
@@ -23,16 +24,22 @@ def on_mouse_down(pos):
         alien.y=random.randint(50,450)
         text="good shot"
         sounds.eep.play()
+
     else:
         #alien.x=random.randint(50,550)
         #alien.y=random.randint(50,450)
         text="almost"
 
 def update():
+    global alien,aliendirection
     alien.x+=3
     if alien.x>=600:
         alien.x=0
-        alien.y=random.randint(5,450)
+    alien.y+=3*aliendirection
+    if alien.y<=0 or alien.y>=500:
+        aliendirection*=-1
+
+    
 
 
 pgzrun.go()
