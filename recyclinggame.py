4129df0ac3c627cb215aac9090b3008be519264a
Nusaybah-web <nusaybahfatin@gmail.com>
2025-08-10 10:31:41 +0200

    Add files via upload

--- recyclinggame.py
+++ recyclinggame.py
@@ -5,7 +5,11 @@ HEIGHT=400
 
 bad=["battery","bag","bottle","chips"]
 
-levels=10
+ani=[]
+
+cl=1
+
+levels=6
 
 win=False
 
@@ -17,31 +21,36 @@ def draw():
     screen.blit("hand",(0,0))
     if win:
         screen.fill("green")
-        screen.draw.text("YOU WIN!!!",center=(605,450),Fontsize=400)
+        screen.draw.text("YOU WIN!!!",center=(300,200),fontsize=40)
     elif loose:
-        screen.fill("green")
-        screen.draw.text("you loose",center=(605,450),Fontsize=400)
+        screen.fill("red")
+        screen.draw.text("you loose",center=(300,200),fontsize=40)
     else:
         for i in items:
             i.draw()
 
+def update():
+    global items
+    if len(items)==0:
+        items=rain(cl)
+
 def rain(extra):
-    itemslist=populate(extra)
-    images=things(itemslist)
+    il=populate(extra)
+    images=things(il)
     layout(images)
-    animate(images)
+    anime(images)
     return images
 
 def populate(extra):
-    itemslist=["paper"]
+    il=["paper"]
     for i in range(extra):
         z=random.choice(bad)
-        itemslist.append(z)
-    return itemslist
+        il.append(z)
+    return il
 
-def things(itemslist):
+def things(il):
     images=[]
-    for i in itemslist:
+    for i in il:
         actors=Actor(i)
         images.append(actors)
     return images
@@ -53,6 +62,52 @@ def layout(num):
     for i,j in enumerate(num,1):
         x=gapsize*i
         j.x=x
+
+def anime(images):
+    for i in images:
+        duration=10-cl
+        i.anchor=("center","bottom")
+        copyanime=animate(i,duration=duration,on_finished=gameover,y=HEIGHT)
+        ani.append(copyanime)
+
+def gameover():
+    global loose
+    loose=True
+
+def complete():
+    global items,ani,cl
+    stop(ani)
+    if cl==levels:
+        win==True
+    else:
+        cl+=1
+        items=[]
+        ani=[]
+
+
+
+def on_mouse_down(pos):
+    for i in items:
+        if i.collidepoint(pos):
+            if "paper" in i.image:
+                complete()
+            else:
+                gameover()
+
+def stop(anim):
+    for i in anim:
+        if i.running:
+            i.stop()
+
+
+pgzrun.go()
+                
+
+
+
+
+
+
 
 
 
