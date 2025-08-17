691de644959dc2d4fd9768b7277ca1e4e21e1e5f
Nusaybah-web <nusaybahfatin@gmail.com>
2025-08-17 10:45:58 +0200

    Add files via upload

--- kahoot.py
+++ kahoot.py
@@ -14,12 +14,21 @@ a4=Rect(330,420,290,130)
 timer=Rect(640,120,140,130)
 skip=Rect(640,270,140,280)
 
+answers=[a1,a2,a3,a4]
+
+
 cq=0
 tq=0
+
 
 sbm=f"welcome to kahoot, you are currently on question number {cq}/{tq}"
 
 info=[]
+
+time=10
+
+finished=False
+s=0
 
 def draw():
     screen.fill("black")
@@ -34,6 +43,12 @@ def draw():
     screen.draw.textbox(sbm,sb,color="white")
     screen.draw.textbox(lines[0],q,color="white")
     
+    for i,j in enumerate(answers,1):
+        screen.draw.textbox(lines[i].strip(),j,color="white")
+
+    screen.draw.textbox("skip",skip,color="white")
+    screen.draw.textbox(str(time),timer,color="white")
+
 
 
 def update():
@@ -52,25 +67,45 @@ def readquestions():
     global cq
     cq+=1
     return info.pop(0).split("|")
-    
-    
-questions()
+
+
+def timer2():
+    global time
+    if time:
+        time-=1
+    else:
+        gameover()
+
+def gameover():
+    global finished,lines,time
+    finished=True
+    lose=f"game over, you have {s} correct"
+    lines=[lose,"-","-","-","-",5]
+    time=0
+
+def ca():
+    global s,time,lines
+    s+=1
+    if info:
+        lines=readquestions()
+        time=10
+    else:
+        gameover()
+
+def on_mouse_down(pos):
+    for i,j in enumerate(answers,1):
+        if j.collidepoint(pos):
+            if i is int(lines[5]):
+                ca()
+            else:
+                gameover()
+questions()   
 
 lines=readquestions()
 
 print(lines)
 
-
-
-
-
-
-
-
-
-
-
-
+clock.schedule_interval(timer2,1)
 
 
 pgzrun.go()
