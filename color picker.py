553dc1658e0131dc5485e09cc1e9aa3947a03007
Nusaybah-web <nusaybahfatin@gmail.com>
2026-01-04 10:05:00 +0100

    Add files via upload

--- color picker.py
+++ color picker.py
@@ -8,6 +8,10 @@ class paint:
         self.root = Tk()
         self.pen_button = Button(self.root, text='pen', command=self.use_pen)
         self.pen_button.grid(row=0, column=0)
+        """self.brush_button = Button(self.root, text='brush', command=self.use_brush)
+        self.brush_button.grid(row=0, column=1)"""
+        self.clear_button = Button(self.root, text='clear', command=self.clear_canvas)
+        self.clear_button.grid(row=0, column=1)
         self.color_button = Button(self.root, text='color', command=self.choose_color)
         self.color_button.grid(row=0, column=2)
         self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
@@ -16,6 +20,7 @@ class paint:
         self.choose_size_button.grid(row=0, column=4)
         self.c = Canvas(self.root, bg='white', width=600, height=600)
         self.c.grid(row=1, columnspan=5)
+        self.setup()
     def setup(self):
         self.old_x = None
         self.old_y = None
@@ -27,27 +32,33 @@ class paint:
         self.c.bind('<ButtonRelease-1>', self.reset)
     def use_pen(self):
         self.activate_button(self.pen_button)
+        self.color=self.DEFAULT_COLOR if self.color=="White" else self.color
+    """def use_brush(self):
+        self.activate_button(self.brush_button)"""
     def choose_color(self):
-        self.eraser_on = False
+        """self.eraser_on = False"""
         self.color = askcolor(color=self.color)[1]
     def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
+        self.activate_button(self.eraser_button,""" eraser_mode=True""")
+        self.color="White"
+    def clear_canvas(self):
+        self.c.delete("all")
     def activate_button(self, some_button, eraser_mode=False):
         self.active_button.config(relief=RAISED)
         some_button.config(relief=SUNKEN)
         self.active_button = some_button
-        self.eraser_on = eraser_mode
+        """self.eraser_on = eraser_mode"""
     def paint(self, event):
         self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
+        """paint_color = 'white' if self.eraser_on else self.color"""
         if self.old_x and self.old_y:
             self.c.create_line(self.old_x, self.old_y, event.x, event.y,
                 width=self.line_width,
-               fill=paint_color,
+               fill=self.color,
                capstyle=ROUND, smooth=TRUE,
                splinesteps=36)
-            self.old_x = event.x
-            self.old_y = event.y
+        self.old_x = event.x
+        self.old_y = event.y
     def reset(self, event):
         self.old_x, self.old_y = None, None
 
